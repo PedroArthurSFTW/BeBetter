accc8c6e0f955d4ed438b1f6d319892b9e585dc8
MoisesCaris <moises.caris@edu.unifil.br>
2024-03-14 10:35:35 -0300

    Modificações DB

---
+++ db/metas.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize")
+const database = require("../db")
+
+const Metas = database.define("metas", {
+  id_metas: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome_meta: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descrica_meta: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+})
+module.exports = Metas
