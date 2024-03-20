1bcaaaa8f2b919a84302d7e4e408aacbd67e38ab
MoisesCaris <moises.caris@edu.unifil.br>
2024-03-20 12:46:51 -0300

    Sistema de confirmação de login

--- index.js
+++ index.js
@@ -30,25 +30,3 @@ app.get("/", (req, res) => {
   res.render("index")
 })
 
-async function excluirAtividadesAnteriores() {
-  try {
-    const dataAtual = new Date() // Obtém a data e hora atual
-    dataAtual.setHours(0, 0, 0, 0) // Define a hora como meia-noite
-
-    // Exclui todas as tarefas anteriores à data atual
-    await Tarefas.destroy({ where: { dia_tarefa: { [Op.lt]: dataAtual } } })
-
-    // Exclui todos os hábitos anteriores à data atual
-    await Habitos.destroy({ where: { dia_semana: { [Op.lt]: dataAtual } } })
-
-    console.log("Tarefas e hábitos anteriores foram excluídos com sucesso.")
-  } catch (error) {
-    console.error("Erro ao excluir tarefas e hábitos anteriores:", error)
-  }
-}
-
-// Chama a função para excluir as atividades anteriores uma vez quando o servidor iniciar
-excluirAtividadesAnteriores()
-
-// Agende a execução da função para excluir as atividades anteriores diariamente à meia-noite
-setInterval(excluirAtividadesAnteriores, 24 * 60 * 60 * 1000) // Executa a cada 24 horas
